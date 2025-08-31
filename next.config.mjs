db5d0da99e85524aa4997d0efb387a1500afbd84
MIKHAIL <hypericum90@gmail.com>
2025-08-31 07:52:33 +0400

    edit deploy files

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === 'production';
+const repoName = 'Crypto';
+
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  assetPrefix: isProduction ? `/${repoName}/` : '',
+  basePath: isProduction ? `/${repoName}` : '',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
+};
+
+export default nextConfig;
